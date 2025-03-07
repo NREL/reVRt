5e745bad48325c25ab91c2f8db4b8b498a729546
Gui Castelao <guilherme@castelao.net>
2025-03-07 09:18:15 -0700

    feat: dataset successors

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -17,4 +17,49 @@ impl Dataset {
 
         Ok(Self { store })
     }
+
+    /// Determine the successors of a position.
+    ///
+    /// ToDo:
+    /// - Handle the edges of the array.
+    /// - Include diagonal moves.
+    /// - Weight the cost. Remember that the cost is for a side,
+    ///   thus a diagnonal move has to calculate consider the longer
+    ///   distance.
+    fn successors(&self, Pos(x, y): Pos) -> Vec<(Pos, u64)> {
+        let array = zarrs::array::Array::open(self.store.clone(), "/A").unwrap();
+
+
+        // Cutting off the edges for now.
+        let shape = array.shape();
+        if x == 0 || x >= (shape[0]) || y == 0 || y >= (shape[1]) {
+            return vec![];
+        }
+
+        let subset = zarrs::array_subset::ArraySubset::new_with_ranges(&[(x-1)..(x+2), (y-1)..(y+2)]);
+
+        //let value = array.retrieve_array_subset_ndarray::<f64>(&subset).unwrap();
+        let value = array.retrieve_array_subset_elements::<f64>(&subset).unwrap();
+        dbg!(&value);
+
+        let output = vec![
+            (Pos(x-1, y-1), (value[0] * 1e4) as u64),
+            (Pos(x, y-1), (value[1]* 1e4) as u64),
+            (Pos(x+1, y-1), (value[2]* 1e4) as u64),
+            (Pos(x-1, y), (value[3]* 1e4) as u64),
+            (Pos(x+1, y), (value[5]* 1e4) as u64),
+            (Pos(x-1, y+1), (value[6]* 1e4) as u64),
+            (Pos(x, y+1), (value[7]* 1e4) as u64),
+            (Pos(x+1, y+1), (value[8]* 1e4) as u64),
+        ];
+        output
+
+
+    }
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(u64, u64);
+
 }
