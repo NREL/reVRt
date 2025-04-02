5e68e4f74adbe37fef68d6c0c6036019b76d3ac3
Gui Castelao <guilherme@castelao.net>
2025-04-02 14:28:58 -0600

    feat: Minimalist CLI to support Dataset development

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,13 +1,30 @@
 //! Command line support for RevX-Transmission
 
+use std::path::PathBuf;
+
 use clap::Parser;
+use tracing::{info, trace};
 use tracing_subscriber;
+
+use nrel_transmission::Simulation;
 
 #[derive(Parser)]
 #[command(version, about, author, long_about = None)]
 struct Cli {
     #[arg(short, long, action=clap::ArgAction::Count)]
     verbose: u8,
+
+    #[arg(short, long, value_name = "DATASET")]
+    dataset: PathBuf,
+
+    #[arg(long = "start", value_delimiter = ',', value_name = "START")]
+    start: Vec<usize>,
+
+    #[arg(long = "end", value_delimiter = ',', value_name = "END")]
+    end: Vec<usize>,
+
+    #[arg(long = "cache-size", value_name = "CACHE_SIZE")]
+    cache_size: Option<usize>,
 }
 
 fn main() {
@@ -22,5 +39,23 @@ fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing_level)
         .init();
-    tracing::info!("Verbose level: {}", cli.verbose);
+    info!("Verbose level: {}", cli.verbose);
+
+    trace!("Loading dataset: {:?}", cli.dataset);
+    let mut simulation = Simulation::new(cli.dataset, 250_000_000).unwrap();
+
+    assert_eq!(cli.start.len(), 2);
+    let start = &nrel_transmission::Point(cli.start[0] as u64, cli.start[1] as u64);
+    trace!("Starting point: {:?}", start);
+
+    assert_eq!(cli.end.len(), 2);
+    let end = vec![nrel_transmission::Point(
+        cli.end[0] as u64,
+        cli.end[1] as u64,
+    )];
+    trace!("Ending point: {:?}", end);
+
+    let result = simulation.scout(&[start.clone()], end);
+    println!("Results: {:?}", result);
+    info!("Final solutions: {:?}", result);
 }
