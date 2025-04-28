0fea83a70cdf2afc354e71fbf7c974894d2027d3
ppinchuk <pinchukpaul@gmail.com>
2025-04-28 11:45:24 -0600

    Add fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from pathlib import Path
 import pytest
 
 
+LOGGING_META_FILES = {"log.py", "exceptions.py", "warnings.py"}
+
+
 @pytest.fixture(scope="module")
 def repo_dir():
     """Return Path to top-level repo directory."""
@@ -21,3 +24,32 @@ def test_dir(repo_dir):
 def test_data_dir(test_dir):
     """Return Path to test data directory."""
     return test_dir / "data"
+
+
+@pytest.fixture
+def assert_message_was_logged(caplog):
+    """Assert that a particular (partial) message was logged."""
+    caplog.clear()
+
+    def assert_message(msg, log_level=None, clear_records=False):
+        """Assert that a message was logged."""
+        assert caplog.records
+
+        for record in caplog.records:
+            if msg in record.message:
+                break
+        else:
+            msg = f"{msg!r} not found in log records"
+            raise AssertionError(msg)
+
+        # record guaranteed to be defined b/c of "assert caplog.records"
+        if log_level:
+            assert record.levelname == log_level  # cspell:disable-line
+        assert record.filename not in LOGGING_META_FILES
+        assert record.funcName != "__init__"
+        assert "trev" in record.name
+
+        if clear_records:
+            caplog.clear()
+
+    return assert_message
