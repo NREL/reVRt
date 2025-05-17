3dd5875d4bc10e42c0c43de148e9771e3579d180
ppinchuk <pinchukpaul@gmail.com>
2025-05-17 12:10:19 -0600

    Linter fix

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -50,7 +50,10 @@ fn main() {
     trace!("Starting point: {:?}", start);
 
     assert_eq!(cli.end.len(), 2);
-    let end = vec![nrel_transmission::Point::new(cli.end[0] as u64, cli.end[1] as u64)];
+    let end = vec![nrel_transmission::Point::new(
+        cli.end[0] as u64,
+        cli.end[1] as u64,
+    )];
     trace!("Ending point: {:?}", end);
 
     let result = resolve(
