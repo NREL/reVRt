39116f6d0036b56f2e925d99dfa9586c361ef88a
Gui Castelao <guilherme@castelao.net>
2025-05-19 12:26:40 -0600

    style:

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -50,10 +50,7 @@ fn main() {
     trace!("Starting point: {:?}", start);
 
     assert_eq!(cli.end.len(), 2);
-    let end = vec![reVRt::Point::new(
-        cli.end[0] as u64,
-        cli.end[1] as u64,
-    )];
+    let end = vec![reVRt::Point::new(cli.end[0] as u64, cli.end[1] as u64)];
     trace!("Ending point: {:?}", end);
 
     let result = resolve(
