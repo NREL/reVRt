5fb346a81134fab7b0f0fb85b0bc58a00bd5ab9f
Gui Castelao <guilherme@castelao.net>
2025-06-09 08:55:03 -0600

    feat: Standard bench, dataset creator

---
+++ crates/revrt/benches/standard.rs
@@ -0,0 +1,91 @@
+//! Benchmarking reVRt
+//!
+//! Compare reVRt's performance to guide development and avoid regressions.
+//!
+//! Cases to consider:
+//! - All ones: So we guarantee always the same solution
+//! - Small distance but large cost array: It should be impacted by
+//!   the cost chunk size only.
+//! - Random cost: Would that create too much noise for statistics?
+//! - Too many layers: How well we parallelize between layers.
+//! - Too many paths in the same area: How well we parallelize
+//!    between paths (re-using cost cache).
+//! - Single chunk with reasonable size: How well we parallelize
+//!   calculating the cost.
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use revrt::bench_minimalist;
+
+use ndarray::Array2;
+use rand::Rng;
+
+enum FeaturesType {
+    AllOnes,
+    Random,
+}
+
+fn features_random(ftype: FeaturesType) -> std::path::PathBuf {
+    let ni = 100;
+    let nj = 100;
+    let ci = 4;
+    let cj = 4;
+
+    let tmp_path = tempfile::TempDir::new().unwrap();
+
+    let store: zarrs::storage::ReadableWritableListableStorage = std::sync::Arc::new(
+        zarrs::filesystem::FilesystemStore::new(tmp_path.path())
+            .expect("could not open filesystem store"),
+    );
+
+    zarrs::group::GroupBuilder::new()
+        .build(store.clone(), "/")
+        .unwrap()
+        .store_metadata()
+        .unwrap();
+
+    // Create an array
+    // Remember to remove /cost
+    for array_path in ["/A", "/B", "/C", "/cost"].iter() {
+        let array = zarrs::array::ArrayBuilder::new(
+            vec![ni, nj], // array shape
+            zarrs::array::DataType::Float32,
+            vec![ci, cj].try_into().unwrap(), // regular chunk shape
+            zarrs::array::FillValue::from(zarrs::array::ZARR_NAN_F32),
+        )
+        // .bytes_to_bytes_codecs(vec![]) // uncompressed
+        .dimension_names(["y", "x"].into())
+        // .storage_transformers(vec![].into())
+        .build(store.clone(), array_path)
+        .unwrap();
+
+        // Write array metadata to store
+        array.store_metadata().unwrap();
+
+        let mut a = vec![];
+        match ftype {
+            FeaturesType::AllOnes => {
+                a.resize((ni * nj).try_into().unwrap(), 1.0);
+            }
+            FeaturesType::Random => {
+                let mut rng = rand::rng();
+                for _x in 0..(ni * nj) {
+                    a.push(rng.random_range(0.0..=1.0));
+                }
+            }
+        }
+        let data: Array2<f32> =
+            ndarray::Array::from_shape_vec((ni.try_into().unwrap(), nj.try_into().unwrap()), a)
+                .unwrap();
+
+        array
+            .store_chunks_ndarray(
+                &zarrs::array_subset::ArraySubset::new_with_ranges(&[0..(ni / ci), 0..(nj / cj)]),
+                data,
+            )
+            .unwrap();
+    }
+
+    tmp_path.keep()
+}
