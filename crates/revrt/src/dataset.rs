9034e97876d8ec80f7a92a5fcf490e5074f91b1f
Gui Castelao <guilherme@castelao.net>
2025-09-04 17:35:09 -0600

    style:

--- crates/revrt/src/dataset.rs
+++ crates/revrt/src/dataset.rs
@@ -10,9 +10,9 @@ use zarrs::storage::{
     ListableStorageTraits, ReadableListableStorage, ReadableWritableListableStorage,
 };
 
+use crate::ArrayIndex;
 use crate::cost::CostFunction;
 use crate::error::Result;
-use crate::ArrayIndex;
 
 /// Manages the features datasets and calculated total cost
 pub(super) struct Dataset {
@@ -197,8 +197,7 @@ impl Dataset {
             for cj in chunks.start()[1]..(chunks.start()[1] + chunks.shape()[1]) {
                 trace!(
                     "Checking if cost for chunk ({}, {}) has been calculated",
-                    ci,
-                    cj
+                    ci, cj
                 );
                 if self.cost_chunk_idx.read().unwrap()[[ci as usize, cj as usize]] {
                     trace!("Cost for chunk ({}, {}) already calculated", ci, cj);
@@ -212,8 +211,7 @@ impl Dataset {
                     if chunk_idx[[ci as usize, cj as usize]] {
                         trace!(
                             "Cost for chunk ({}, {}) already calculated while waiting for the lock",
-                            ci,
-                            cj
+                            ci, cj
                         );
                     } else {
                         self.calculate_chunk_cost(ci, cj);
