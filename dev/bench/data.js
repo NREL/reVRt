fb19c990325dfe23aa145a389b69f7dccf5b884d
github-action-benchmark <github@users.noreply.github.com>
2025-09-26 01:11:39 +0000

    add Rust Benchmark (cargo) benchmark result for 09b132485e9ee74bff75fde449e8eb4039623a61

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1758849099405,
+  "repoUrl": "https://github.com/NREL/reVRt",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "guilherme@castelao.net",
+            "name": "Guilherme Castelão",
+            "username": "castelao"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09b132485e9ee74bff75fde449e8eb4039623a61",
+          "message": "Fixing PyPI release (#144)\n\n* style:\n\n* Checkout full history",
+          "timestamp": "2025-09-25T18:59:10-06:00",
+          "tree_id": "6bba0d0b215c59b526ada1778e6edf4f1284842b",
+          "url": "https://github.com/NREL/reVRt/commit/09b132485e9ee74bff75fde449e8eb4039623a61"
+        },
+        "date": 1758849098091,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "constant_cost",
+            "value": 73066683,
+            "range": "± 1179868",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "random_cost",
+            "value": 74283287,
+            "range": "± 1116727",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "single_chunk",
+            "value": 431264723,
+            "range": "± 18109948",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/0",
+            "value": 139240286,
+            "range": "± 3140447",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/1",
+            "value": 141815945,
+            "range": "± 3070035",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/2",
+            "value": 150274383,
+            "range": "± 1325144",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/5",
+            "value": 179961911,
+            "range": "± 1749386",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/10",
+            "value": 279096376,
+            "range": "± 4374667",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
