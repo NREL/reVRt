b8871eb0c28a0df0e85dd237e9082f19c34a2d6b
Gui Castelao <guilherme@castelao.net>
2025-10-11 18:39:07 -0600

    feat: A struct to store the routing solution

---
+++ crates/revrt/src/solution.rs
@@ -0,0 +1,27 @@
+//! Routing solution representation
+
+#[allow(missing_docs, dead_code)]
+#[derive(Debug)]
+/// Solution for one single routing case
+///
+pub(super) struct Solution<I, C> {
+    route: Vec<I>,
+    total_cost: C,
+}
+
+impl<I, C> Solution<I, C> {
+    #[allow(dead_code, missing_docs)]
+    pub(crate) fn new(route: Vec<I>, total_cost: C) -> Self {
+        Self { route, total_cost }
+    }
+
+    #[allow(dead_code, missing_docs)]
+    pub(super) fn route(&self) -> &Vec<I> {
+        &self.route
+    }
+
+    #[allow(dead_code, missing_docs)]
+    pub(super) fn total_cost(&self) -> &C {
+        &self.total_cost
+    }
+}
