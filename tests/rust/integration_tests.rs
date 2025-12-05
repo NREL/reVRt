e91e22fc2cf9f84049f3ec7a2e337214a85b346b
ppinchuk <pinchukpaul@gmail.com>
2025-12-05 11:32:22 -0700

    Add integration test

---
+++ tests/rust/integration_tests.rs
@@ -0,0 +1,26 @@
+use revrt::resolve;
+use std::path::PathBuf;
+
+const TEST_DATA: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/../../tests/data/utilities/transmission_layers.zarr"
+);
+
+#[test]
+fn basic_routing_in_data() {
+    let layers_path = PathBuf::from(TEST_DATA);
+    let start = &revrt::ArrayIndex::new(10, 10);
+    let end = vec![revrt::ArrayIndex::new(20, 20)];
+    let result = resolve(
+        layers_path.to_str().expect("test data path is valid UTF-8"),
+        r#"{"cost_layers": [{"layer_name": "tie_line_costs_102MW"}]}"#,
+        250_000_000,
+        std::slice::from_ref(start),
+        end,
+    )
+    .unwrap();
+    dbg!(&result);
+    assert_eq!(result.len(), 1);
+    assert!(result[0].0.len() > 1);
+    assert!(result[0].1 > 0.);
+}
