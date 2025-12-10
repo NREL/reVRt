54ca855da970f11c2cc108ac5fe60de06947f480
ppinchuk <pinchukpaul@gmail.com>
2025-12-09 21:57:44 -0700

    Add first pass of cli file

---
+++ revrt/routing/cli.py
@@ -0,0 +1,464 @@
+"""reVRt routing CLI functions"""
+
+import time
+import logging
+import contextlib
+from math import ceil
+from pathlib import Path
+from copy import deepcopy
+
+import pandas as pd
+import geopandas as gpd
+import xarray as xr
+from gaps.cli import CLICommandFromFunction
+
+from revrt.costs.config import parse_config
+from revrt.routing.point_to_many import find_all_routes, RoutingScenario
+from revrt.routing.utilities import map_to_costs
+
+
+logger = logging.getLogger(__name__)
+_MILLION_USD_PER_MILE_TO_USD_PER_PIXEL = 55923.40730136006
+"""Conversion from million dollars/mile to $/pixel
+
+1,000,000 [$/million dollars]
+* 90 [meters/pixel]
+/ 1609.344 [meters/mile]
+= 55923.40730136006 [$/pixel]
+"""
+
+
+def compute_lcp_routes(  # noqa: PLR0913, PLR0917
+    cost_fpath,
+    route_table,
+    cost_layers,
+    out_dir,
+    job_name,
+    friction_layers=None,
+    tracked_layers=None,
+    cost_multiplier_layer=None,
+    cost_multiplier_scalar=1,
+    transmission_config=None,
+    save_paths=False,
+    use_hard_barrier=False,
+    _split_params=None,
+):
+    """_summary_
+
+    Parameters
+    ----------
+    cost_fpath : path-like
+        Path to layered Zarr file containing cost and other required
+        routing layers.
+    route_table : path-like
+        Path to CSV file defining the start and
+        end points of all routes. Must have the following columns:
+
+            "start_lat": Stating point latitude
+            "start_lon": Stating point longitude
+            "end_lat": Ending point latitude
+            "end_lon": Ending point longitude
+
+    cost_layers : list
+        List of H5 layers that are summed to determine total costs
+        raster used for routing. Costs and distances for each individual
+        layer are also reported as requested (e.g. wet and dry costs).
+        Each dict in the list should have the following keys:
+
+            - "layer_name": (REQUIRED) Name of layer in layered file
+              containing cost data.
+            - "multiplier_layer": (OPTIONAL) Name of layer in layered
+              file containing spatially explicit multiplier values to
+              apply to this cost layer before summing it with the
+              others. Default is ``None``.
+            - "multiplier_scalar": (OPTIONAL) Scalar value to multiply
+              this layer by before summing it with the others. Default
+              is ``1``.
+            - "is_invariant": (OPTIONAL) Boolean flag indicating whether
+              this layer is length invariant (i.e. should NOT be
+              multiplied by path length; values should be $). Default is
+              ``False``.
+            - "include_in_report": (OPTIONAL) Boolean flag indicating
+              whether the costs and distances for this layer should be
+              output in the final LCP table. Default is ``True``.
+            - "apply_row_mult": (OPTIONAL) Boolean flag indicating
+              whether the right-of-way width multiplier should be
+              applied for this layer. If ``True``, then the transmission
+              config should have a "row_width" dictionary that maps
+              voltages to right-of-way width multipliers. Also, the
+              routing table input should have a "voltage" entry for
+              every route. Every "voltage" value in the routing table
+              must be given in the "row_width" dictionary in the
+              transmission config, otherwise an error will be thrown.
+              Default is ``False``.
+            - "apply_polarity_mult": (OPTIONAL) Boolean flag indicating
+              whether the polarity multiplier should be applied for this
+              layer. If ``True``, then the transmission config should
+              have a "voltage_polarity_mult" dictionary that maps
+              voltages to a new dictionary, the latter mapping
+              polarities to multipliers. For example, a valid
+              "voltage_polarity_mult" dictionary might be
+              ``{"138": {"ac": 1.15, "dc": 2}}``.
+              In addition, the routing table input should have a
+              "voltage" **and** a "polarity" entry for every route.
+              Every "voltage" + "polarity" combination in the routing
+              table must be given in the "voltage_polarity_mult"
+              dictionary in the transmission config, otherwise an error
+              will be thrown.
+
+              ..IMPORTANT::
+                The multiplier in this config is assumed to be in units
+                of million $ per mile and will be converted to
+                $ per pixel before being applied to the layer!.
+
+              Default is ``False``.
+
+    out_dir : path-like
+        Directory to store routing outputs.
+    job_name : str
+        Name to use for this routing job. This name will be used to
+        construct output file names.
+    friction_layers : list, optional
+        Layers to be added to costs to influence routing but NOT
+        reported in final cost (i.e. friction, barriers, etc.). Each
+        item in this list should be a dictionary containing the
+        following keys:
+
+            - "layer_name": (REQUIRED) Name of layer in layered file
+              containing routing data. This can also be "lcp_agg_costs",
+              which represents the layer built out using the
+              `cost_layers` input.
+            - "multiplier_layer": (OPTIONAL) Name of layer in layered
+              file containing spatially explicit multiplier values to
+              apply to this routing layer before summing it with the
+              others. Default is ``None``. This can also be
+              "lcp_agg_costs", which represents the layer built out
+              using the `cost_layers` input.
+            - "multiplier_scalar": (OPTIONAL) Scalar value to multiply
+              this layer by before summing it with the others. Default
+              is ``1``.
+            - "include_in_report": (OPTIONAL) Boolean flag indicating
+              whether the routing and distances for this layer should be
+              output in the final LCP table. Default is ``False``.
+            - "apply_row_mult": (OPTIONAL) Boolean flag indicating
+              whether the right-of-way width multiplier should be
+              applied for this layer. If ``True``, then the transmission
+              config should have a "row_width" dictionary that maps
+              voltages to right-of-way width multipliers. Also, the
+              routing table input should have a "voltage" entry for
+              every route. Every "voltage" value in the routing table
+              must be given in the "row_width" dictionary in the
+              transmission config, otherwise an error will be thrown.
+              Default is ``False``.
+            - "apply_polarity_mult": (OPTIONAL) Boolean flag indicating
+              whether the polarity multiplier should be applied for this
+              layer. If ``True``, then the transmission config should
+              have a "voltage_polarity_mult" dictionary that maps
+              voltages to a new dictionary, the latter mapping
+              polarities to multipliers. For example, a valid
+              "voltage_polarity_mult" dictionary might be
+              ``{"138": {"ac": 1.15, "dc": 2}}``.
+              In addition, the routing table input should have a
+              "voltage" **and** a "polarity" entry for every route.
+              Every "voltage" + "polarity" combination in the routing
+              table must be given in the "voltage_polarity_mult"
+              dictionary in the transmission config, otherwise an error
+              will be thrown.
+
+              ..IMPORTANT::
+                The multiplier in this config is assumed to be in units
+                of million $ per mile and will be converted to
+                $ per pixel before being applied to the layer!.
+
+              Default is ``False``.
+
+        By default, ``None``.
+    tracked_layers : dict, optional
+        Dictionary mapping layer names to strings, where the strings are
+        dask (which supports almost all numpy-equivalent) methods that
+        should be applied to the layer along the LCP to be included as a
+        characterization column in the output. By default, ``None``.
+    cost_multiplier_layer : str, optional
+        Name of layer in layered Zarr file containing final cost layer
+        spatial multipliers. By default, ``None``.
+    cost_multiplier_scalar : int, default=1
+        Final cost layer multiplier. By default, ``1``.
+    transmission_config : path-like or dict, optional
+        Dictionary of transmission cost configuration values, or
+        path to JSON/JSON5 file containing this dictionary. The
+        dictionary should have a subset of the following keys:
+
+            - base_line_costs
+            - iso_lookup
+            - iso_multipliers
+            - land_use_classes
+            - new_substation_costs
+            - power_classes
+            - power_to_voltage
+            - transformer_costs
+            - upgrade_substation_costs
+            - voltage_polarity_mult
+            - row_width
+
+        Each of these keys should point to another dictionary or
+        path to JSON/JSON5 file containing a dictionary of
+        configurations for each section. For the expected contents
+        of each dictionary, see the default config. If ``None``,
+        values from the default config are used.
+        By default, ``None``.
+    save_paths : bool, default=False
+        Boolean flag to save the least cost path geometry in the output
+        (i.e. the output file will be a GeoPackage instead of a CSV).
+        By default, ``False``.
+    use_hard_barrier : bool, optional
+        Optional flag to treat any cost values of <= 0 as a hard barrier
+        (i.e. no paths can ever cross this). If ``False``, cost values
+        of <= 0 are set to a large value to simulate a strong but
+        permeable barrier. By default, ``False``.
+
+    Returns
+    -------
+    str or None
+        Path to file containing route data, if any routes were
+        completed.
+    """
+
+    start_time = time.time()
+
+    out_dir = Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    logger.debug("Tracked layers input: %r", tracked_layers)
+    logger.debug("Transmission config input: %r", transmission_config)
+
+    transmission_config = parse_config(config=transmission_config)
+    logger.debug("Transmission Config: %s", transmission_config)
+
+    inds = _get_indices(
+        route_table,
+        sort_cols=["start_lat", "start_lon"],
+        split_params=_split_params,
+    )
+    if len(inds) == 0:
+        logger.info("No indices to process: %s", inds)
+        return None
+
+    with contextlib.suppress(TypeError, UnicodeDecodeError):
+        route_points = pd.read_csv(route_table)
+
+    if inds is not None:
+        route_points = route_points.loc[inds]
+
+    out_fp = (
+        out_dir / f"{job_name}.gpkg"
+        if save_paths
+        else out_dir / f"{job_name}.csv"
+    )
+
+    _run_lcp(
+        cost_fpath,
+        route_points,
+        cost_layers,
+        out_fp=out_fp,
+        transmission_config=transmission_config,
+        cost_multiplier_layer=cost_multiplier_layer,
+        cost_multiplier_scalar=cost_multiplier_scalar,
+        friction_layers=friction_layers,
+        tracked_layers=tracked_layers,
+        use_hard_barrier=use_hard_barrier,
+    )
+
+    elapsed_time = (time.time() - start_time) / 60
+    logger.info("Processing took %.2f minutes", elapsed_time)
+
+    return str(out_fp)
+
+
+def _run_lcp(
+    cost_fpath,
+    route_points,
+    cost_layers,
+    out_fp,
+    transmission_config=None,
+    cost_multiplier_layer=None,
+    cost_multiplier_scalar=1,
+    friction_layers=None,
+    tracked_layers=None,
+    use_hard_barrier=True,
+):
+    ts = time.monotonic()
+    out_fp = Path(out_fp)
+    save_paths = out_fp.suffix.lower() == ".gpkg"
+
+    if route_points.empty:
+        logger.info("Found no paths to compute!")
+
+        return
+
+    with xr.open_dataset(cost_fpath, consolidated=False, engine="zarr") as ds:
+        route_points = map_to_costs(
+            route_points,
+            crs=ds.rio.crs,
+            transform=ds.rio.transform(),
+            shape=ds.rio.shape,
+        )
+
+    logger.info("Computing Least Cost Paths")
+    num_computed = 0
+    for polarity, voltage, routes in _paths_to_compute(route_points, out_fp):
+        route_cl = _update_multipliers(
+            cost_layers, polarity, voltage, transmission_config
+        )
+        route_fl = _update_multipliers(
+            friction_layers or [], polarity, voltage, transmission_config
+        )
+        route_definitions = [
+            (
+                (info["start_row"], info["start_col"]),
+                [(info["end_row"], info["end_col"])],
+                info.to_dict(),
+            )
+            for __, info in routes.iterrows()
+        ]
+
+        scenario = RoutingScenario(
+            cost_fpath=cost_fpath,
+            cost_layers=route_cl,
+            friction_layers=route_fl,
+            tracked_layers=tracked_layers,
+            cost_multiplier_layer=cost_multiplier_layer,
+            cost_multiplier_scalar=cost_multiplier_scalar,
+            use_hard_barrier=use_hard_barrier,
+        )
+
+        out = find_all_routes(
+            scenario,
+            route_definitions=route_definitions,
+            save_paths=save_paths,
+        )
+
+        num_computed += len(out)
+
+        if save_paths:
+            gpd.GeoDataFrame(out).to_file(out_fp, driver="GPKG", mode="a")
+        else:
+            pd.DataFrame(out).to_csv(out_fp, mode="a")
+
+    time_elapsed = f"{(time.monotonic() - ts) / 3600:.4f} hour(s)"
+    logger.info("%d paths were computed in %s", num_computed, time_elapsed)
+
+
+def _paths_to_compute(route_points, out_fp):
+    """Iterate over the paths that should be computed
+
+    Filters out any routes that are already present in `out_fp`. If all
+    routes in a group already exist, the group is skipped.
+    """
+    existing_routes = _collect_existing_routes(out_fp)
+
+    group_cols = ["start_row", "start_col", "polarity", "voltage"]
+    for check_col in ["polarity", "voltage"]:
+        if check_col not in route_points.columns:
+            route_points[check_col] = "unknown"
+
+    for group_info, routes in route_points.groupby(group_cols):
+        if existing_routes:
+            mask = routes.apply(
+                lambda row: (
+                    int(row["start_row"]),
+                    int(row["start_col"]),
+                    int(row["end_row"]),
+                    int(row["end_col"]),
+                    str(row.get("polarity", "unknown")),
+                    str(row.get("voltage", "unknown")),
+                )
+                not in existing_routes,
+                axis=1,
+            )
+            routes = routes[mask]  # noqa: PLW2901
+
+        if routes.empty:
+            continue
+
+        *__, polarity, voltage = group_info
+        yield polarity, voltage, routes
+
+
+def _collect_existing_routes(out_fp):
+    if out_fp is None or not out_fp.exists():
+        return set()
+
+    if out_fp.suffix.lower() == ".gpkg":
+        existing_df = gpd.read_file(out_fp)
+    else:
+        existing_df = pd.read_csv(out_fp)
+
+    return {
+        (
+            int(row["start_row"]),
+            int(row["start_col"]),
+            int(row["end_row"]),
+            int(row["end_col"]),
+            str(row.get("polarity", "unknown")),
+            str(row.get("voltage", "unknown")),
+        )
+        for __, row in existing_df.iterrows()
+    }
+
+
+def _update_multipliers(layers, polarity, voltage, transmission_config):
+    """update layer multipliers based on user input"""
+    output_layers = deepcopy(layers)
+    polarity = str(polarity)
+    voltage = str(int(voltage)) if voltage != "unknown" else "unknown"
+
+    for layer in output_layers:
+        if layer.pop("apply_row_mult", False):
+            row_multiplier = transmission_config["row_width"][voltage]
+            layer["multiplier_scalar"] = (
+                layer.get("multiplier_scalar", 1) * row_multiplier
+            )
+
+        if layer.pop("apply_polarity_mult", False):
+            polarity_config = transmission_config["voltage_polarity_mult"]
+            polarity_multiplier = polarity_config[voltage][polarity]
+            layer["multiplier_scalar"] = (
+                layer.get("multiplier_scalar", 1)
+                * polarity_multiplier
+                * _MILLION_USD_PER_MILE_TO_USD_PER_PIXEL
+            )
+
+    return output_layers
+
+
+def _get_indices(features_fp, sort_cols, split_params):
+    """Get indices of points that are sorted by location"""
+    features = gpd.read_file(features_fp)
+    features = features.sort_values(sort_cols)
+
+    start_ind, n_chunks = split_params or (0, 1)
+    chunk_size = ceil(len(features) / n_chunks)
+    return features.index[
+        start_ind * chunk_size : (start_ind + 1) * chunk_size
+    ]
+
+
+def _split_routes(config):
+    """Compute route split params inside of config"""
+    exec_control = config.get("execution_control", {})
+    if exec_control.get("option") == "local":
+        num_nodes = 1
+    else:
+        num_nodes = exec_control.pop("nodes", 1)
+
+    config["_split_params"] = [(i, num_nodes) for i in range(num_nodes)]
+    return config
+
+
+route_points_command = CLICommandFromFunction(
+    compute_lcp_routes,
+    name="route-points",
+    add_collect=False,
+    split_keys={"_split_params"},
+    config_preprocessor=_split_routes,
+)
