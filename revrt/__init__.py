568272ea3ac373fb824e9d7d2abafff4ee8851bf
Paul Pinchuk <ppinchuk@nrel.gov>
2025-12-10 11:50:36 -0700

    Make sure Rust-based `find_paths` function is included in docs (#170)
    * Update docstring
    
    * Add Section delimiter to template
    
    * Get function to be picked up by autodoc
    
    * Document reason for adjustment

--- revrt/__init__.py
+++ revrt/__init__.py
@@ -4,5 +4,7 @@ import importlib.metadata
 
 from ._rust import find_paths
 
+# Needed for inclusion in Sphinx autodoc documentation generation
+find_paths.__module__ = "revrt"
 
 __version__ = version = importlib.metadata.version("NREL-reVRt")
