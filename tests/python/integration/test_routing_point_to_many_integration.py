6e7ec605966fbfd3f093d970b548f1fe4c0d6543
ppinchuk <pinchukpaul@gmail.com>
2025-12-16 13:10:09 -0700

    Add integration tets for soft barrier in large file

---
+++ tests/python/integration/test_routing_point_to_many_integration.py
@@ -0,0 +1,90 @@
+"""reVrt tests for routing utilities"""
+
+from pathlib import Path
+
+import numpy as np
+import pytest
+import xarray as xr
+from rasterio.transform import from_origin
+
+from revrt.utilities import LayeredFile
+from revrt.routing.point_to_many import find_all_routes, RoutingScenario
+
+
+@pytest.fixture(scope="module")
+def sample_large_layered_data(tmp_path_factory):
+    """Sample (large) layered data files to use across tests"""
+    data_dir = tmp_path_factory.mktemp("routing_data")
+
+    layered_fp = data_dir / "test_large_layered.zarr"
+    layer_file = LayeredFile(layered_fp)
+
+    height, width = (1000, 1000)
+    cell_size = 1.0
+    x0, y0 = 0.0, float(height)
+    transform = from_origin(x0, y0, cell_size, cell_size)
+    x_coords = (
+        x0 + np.arange(width, dtype=np.float32) * cell_size + cell_size / 2
+    )
+    y_coords = (
+        y0 - np.arange(height, dtype=np.float32) * cell_size - cell_size / 2
+    )
+
+    layer_1 = -1 * np.ones((1, height, width), dtype=np.float32)
+    layer_1[0, 5, 0] = 1000
+    layer_1[0, 5, 900] = 100
+
+    for ind, routing_layer in enumerate(
+        [
+            layer_1,
+        ],
+        start=1,
+    ):
+        da = xr.DataArray(
+            routing_layer,
+            dims=("band", "y", "x"),
+            coords={"y": y_coords, "x": x_coords},
+        )
+        da = da.rio.write_crs("EPSG:4326")
+        da = da.rio.write_transform(transform)
+
+        geotiff_fp = data_dir / f"layer_{ind}.tif"
+        da.rio.to_raster(geotiff_fp, driver="GTiff")
+
+        layer_file.write_geotiff_to_file(
+            geotiff_fp, f"layer_{ind}", overwrite=True
+        )
+    return layered_fp
+
+
+@pytest.mark.parametrize("use_hard_barrier", [True, False])
+def test_soft_barrier(sample_large_layered_data, use_hard_barrier):
+    """Test that soft barriers work as expected in point-to-many routing"""
+    scenario = RoutingScenario(
+        cost_fpath=sample_large_layered_data,
+        cost_layers=[{"layer_name": "layer_1"}],
+        use_hard_barrier=use_hard_barrier,
+    )
+
+    output = find_all_routes(
+        scenario,
+        route_definitions=[
+            ((5, 0), [(5, 900)], {}),
+        ],
+        save_paths=True,
+    )
+    if use_hard_barrier:
+        assert isinstance(output, list)
+        assert not output
+    else:
+        assert len(output) == 1
+        route = output[0]
+        assert route["cost"] == pytest.approx(550.0)
+        assert route["length_km"] == pytest.approx(0.9)
+        x, y = route["geometry"].xy
+        assert np.allclose(x, np.linspace(0.5, 900.5, num=901))
+        assert np.allclose(y, 994.5)
+
+
+if __name__ == "__main__":
+    pytest.main(["-q", "--show-capture=all", Path(__file__), "-rapP"])
