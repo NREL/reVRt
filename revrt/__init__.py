efbfb1aba47aa9071b42149eb7e27182c6a3e245
ppinchuk <pinchukpaul@gmail.com>
2025-12-22 22:07:21 -0700

    Include class in documentation

--- revrt/__init__.py
+++ revrt/__init__.py
@@ -6,5 +6,6 @@ from ._rust import find_paths, RouteFinder
 
 # Needed for inclusion in Sphinx autodoc documentation generation
 find_paths.__module__ = "revrt"
+RouteFinder.__module__ = "revrt"
 
 __version__ = version = importlib.metadata.version("NREL-reVRt")
