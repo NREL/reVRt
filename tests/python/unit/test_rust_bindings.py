557190ae68a614001c0ca20cc879c77ba9331af8
ppinchuk <pinchukpaul@gmail.com>
2025-12-22 23:09:15 -0700

    Add python-side tests for `RouteFinder`

--- tests/python/unit/test_rust_bindings.py
+++ tests/python/unit/test_rust_bindings.py
@@ -8,10 +8,10 @@ import numpy as np
 import xarray as xr
 from skimage.graph import MCP_Geometric
 
-from revrt import find_paths
+from revrt import find_paths, RouteFinder
 
 
-def test_basic_single_route(tmp_path):
+def test_find_paths_basic_single_route(tmp_path):
     """Test a basic routing invocation"""
 
     da = xr.DataArray(
@@ -50,6 +50,57 @@ def test_basic_single_route(tmp_path):
 
     assert len(results) == 1
     test_path, test_cost = results[0]
+
+    mcp = MCP_Geometric(da.values[0])
+    costs, __ = mcp.find_costs(starts=[(1, 1)], ends=[(2, 6)])
+
+    assert test_path == mcp.traceback((2, 6))
+    assert np.isclose(test_cost, costs[(2, 6)])
+
+
+def test_route_finder_basic_single_route(tmp_path):
+    """Test a basic routing invocation"""
+
+    da = xr.DataArray(
+        np.array(
+            [
+                [
+                    [7, 7, 8, 0, 9, 9, 9, 0],
+                    [8, 1, 2, 2, 9, 9, 9, 0],
+                    [9, 1, 3, 3, 9, 1, 2, 3],
+                    [9, 1, 2, 1, 9, 1, 9, 0],
+                    [9, 9, 9, 1, 9, 1, 9, 0],
+                    [9, 9, 9, 1, 1, 1, 9, 0],
+                    [9, 9, 9, 9, 9, 9, 9, 0],
+                ]
+            ],
+            dtype=np.float32,
+        ),
+        dims=("band", "y", "x"),
+    )
+
+    test_cost_fp = tmp_path / "test.zarr"
+    ds = xr.Dataset({"test_costs": da})
+    ds["test_costs"].encoding = {"fill_value": 1_000.0, "_FillValue": 1_000.0}
+    ds.chunk({"x": 4, "y": 3}).to_zarr(test_cost_fp, mode="w", zarr_format=3)
+
+    cost_definition = {"cost_layers": [{"layer_name": "test_costs"}]}
+    routing_results = RouteFinder(
+        zarr_fp=test_cost_fp,
+        cost_function=json.dumps(cost_definition),
+        route_definitions=[
+            (2, [(1, 1)], [(2, 6)]),
+            (4, [(1, 2)], [(1000, 1000)]),
+        ],
+    )
+
+    for route_id, solutions in routing_results:
+        if route_id == 4:
+            assert len(solutions) == 0
+        else:
+            assert route_id == 2
+            assert len(solutions) == 1
+            test_path, test_cost = solutions[0]
 
     mcp = MCP_Geometric(da.values[0])
     costs, __ = mcp.find_costs(starts=[(1, 1)], ends=[(2, 6)])
