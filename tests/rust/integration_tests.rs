198ccff8d34a14674a9d9f0ae28f0776d07e72ea
ppinchuk <pinchukpaul@gmail.com>
2025-12-27 16:15:57 -0700

    Add another integration test

--- tests/rust/integration_tests.rs
+++ tests/rust/integration_tests.rs
@@ -24,3 +24,27 @@ fn basic_routing_in_data() {
     assert!(result[0].route().len() > 1);
     assert!(result[0].total_cost() > &0.);
 }
+
+#[test]
+fn basic_routing_in_data_with_friction() {
+    let layers_path = PathBuf::from(TEST_DATA);
+    let start = &revrt::ArrayIndex::new(10, 10);
+    let end = vec![revrt::ArrayIndex::new(20, 20)];
+    let result = resolve(
+        layers_path.to_str().expect("test data path is valid UTF-8"),
+        r#"{
+            "cost_layers": [{"layer_name": "tie_line_costs_102MW"}],
+            "friction_layers": [
+                {"multiplier_layer": "transmission_barrier", "multiplier_scalar": 100}
+            ]
+        }"#,
+        250_000_000,
+        std::slice::from_ref(start),
+        end,
+    )
+    .unwrap();
+    dbg!(&result);
+    assert_eq!(result.len(), 1);
+    assert!(result[0].route().len() > 1);
+    assert!(result[0].total_cost() > &0.);
+}
